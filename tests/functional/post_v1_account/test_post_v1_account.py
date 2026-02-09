32e462bf7f5b7b6898ca2f2147c45f87c9fc9947
BobcatJo <stromtelecom@yandex.ru>
2026-02-09 19:22:54 +0300

    +добавлен конфигуратор

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,9 +1,9 @@
-from json import loads
 
-import requests
 from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
 from api_mail.apis.mail_api import MailApi
+from restclient.configuration import Configuration as MailConfiguration
+from restclient.configuration import Configuration as DmApiConfiguration
 import structlog
 structlog.configure(
     processors=[
@@ -18,11 +18,14 @@ structlog.configure(
 
 def test_post_v1_account():
     # Регистрация пользователя
-    account_api = AccountApi(host='http://185.185.143.231:5051')
-    login_api = LoginApi(host='http://185.185.143.231:5051')
-    mail_api = MailApi(host='http://185.185.143.231:5025')
+    mail_configuration = MailConfiguration(host='http://185.185.143.231:5025')
+    dm_api_configuration = DmApiConfiguration(host='http://185.185.143.231:5051',disable_log=False)
 
-    login = '2cKksq26'
+    account_api = AccountApi(configuration=dm_api_configuration)
+    login_api = LoginApi(configuration=dm_api_configuration)
+    mail_api = MailApi(configuration=mail_configuration)
+
+    login = 'cKksq200'
     password = 'alex_1'
     email = f'{login}@ya.ru'
 
--- tests/functional/post_v1_account/test_put_v1_account_token.py
+++ tests/functional/post_v1_account/test_put_v1_account_token.py
@@ -4,6 +4,8 @@ import requests
 from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
 from api_mail.apis.mail_api import MailApi
+from restclient.configuration import Configuration as MailConfiguration
+from restclient.configuration import Configuration as DmApiConfiguration
 import structlog
 structlog.configure(
     processors=[
@@ -17,11 +19,14 @@ structlog.configure(
 
 def test_post_v1_account():
     # Регистрация пользователя
-    account_api = AccountApi(host='http://185.185.143.231:5051')
-    login_api = LoginApi(host='http://185.185.143.231:5051')
-    mail_api = MailApi(host='http://185.185.143.231:5025')
+    mail_configuration = MailConfiguration(host='http://185.185.143.231:5025')
+    dm_api_configuration = DmApiConfiguration(host='http://185.185.143.231:5051', disable_log=False)
 
-    login = '1_1'
+    account_api = AccountApi(configuration=dm_api_configuration)
+    login_api = LoginApi(configuration=dm_api_configuration)
+    mail_api = MailApi(configuration=mail_configuration)
+
+    login = '1_12241'
     password = 'alex_1'
     email = f'{login}@ya.ru'
 
@@ -31,25 +36,18 @@ def test_post_v1_account():
         'password': password,
     }
     response = account_api.post_v1_account(json_data=json_data)
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 201, f'Пользователь не создан {response.json()}'
 
     # Получение письма
     response = mail_api.get_api_v2_messages()
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 200, "Письмо не получено"
 
     # Получение токена
     token = get_activation_token_by_login(login, response)
-    print(token)
     assert token is not None, f"Токен не был получен для пользователя {login}"
 
     # Активация пользователя
     response = account_api.put_v1_account_token(token = token)
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 200, f"Пользователь {login},не был активирован"
 
 def get_activation_token_by_login(login, response):
@@ -58,6 +56,5 @@ def get_activation_token_by_login(login, response):
         user_data = loads(item['Content']['Body'])
         user_login = user_data['Login']
         if user_login == login:
-            print(user_login)
             token = user_data['ConfirmationLinkUrl'].split('/')[-1]
     return token
