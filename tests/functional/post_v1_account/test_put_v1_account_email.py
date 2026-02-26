b01f3e11ab46ffa2e31c04399fce1bc93ea7e657
BobcatJo <stromtelecom@yandex.ru>
2026-02-26 20:45:13 +0300

    +fix

--- tests/functional/post_v1_account/test_put_v1_account_email.py
+++ tests/functional/post_v1_account/test_put_v1_account_email.py
@@ -8,7 +8,7 @@ import allure
 def test_put_v1_account_email(account_helper, mail_api):
     # Изменение email пользователя
 
-    login = 'zx_c38a3f03-78ee-45dd-9b67-efcd2b8ec562'
+    login = 'zx_344eac14-4488-4ad6-88cc-ee156ef5ef01'
     password = 'alex_1'
     email = f'{login}@ya.ru'
 
